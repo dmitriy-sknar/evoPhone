187876de06536c1bc373ede79873fb600d3d3fc6
DTSR <DTSR@DK01WV6069>
2019-03-04 10:47:45 +0200

    Add project files.

---
+++ evoPhone.biz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evoPhone.biz {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
